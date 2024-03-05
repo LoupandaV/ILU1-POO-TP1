5505ae576e9bf8f64443b7aa37edfedca04fff78
Leo Gayraud <grl5123b@ensfsi.onmicrosoft.com>
2024-03-05 09:03:48 +0100

    fin du TP2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,11 +3,15 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement;
 	
 	public Romain(String nom, int force) {
 		this.nom = nom;
-		this.force = force;
 		assert force > 0;
+		this.force = force;
+		equipements= new Equipement[2];
+		nbEquipement=0;
 	}
 	
 	public String getNom() {
@@ -23,17 +27,53 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
-		force -= forceCoup;
 		assert force > 0;
-		if (force > 0) {
+		int forceActuelle = force;
+		if ((force - forceCoup) < 0) {
+			force = 0;
 			parler("Aie");
 		} else {
+			force -= forceCoup;
 			parler("J'abandonne...");
 		}
+		assert force < forceActuelle;
 	}
 	
+	public void sEquiper(Equipement equipement) {
+		if (nbEquipement>=2) {
+			parlerSoldat("est déja bien protégé !");
+		} 
+		else if (nbEquipement > 0 && equipements[nbEquipement-1]== equipement) {
+			parlerSoldat("possède déja un "+equipement+".");
+			
+		}
+		else {
+			equipements[nbEquipement]=equipement;
+			nbEquipement+=1;
+			parlerSoldat("s'équipe avec un "+ equipement+".");
+			}
+		}
+
+	
+	
+
+	private void parlerSoldat(String message) {
+		System.out.println( "Le soldat " + nom + " "+message);
+	}
+	
+	
+	
 	public static void main(String[] args) {
-		assert 1<0;
+		Romain minus = new Romain("Minus", 6);
+		Equipement casque= Equipement.CASQUE;
+		Equipement bouclier= Equipement.BOUCLIER;
+		minus.sEquiper(casque);
+		minus.sEquiper(casque);
+		minus.sEquiper(bouclier);
+		minus.sEquiper(bouclier);
+		
+				
+		
 	}
 
 }
